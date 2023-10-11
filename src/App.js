d32e71beb2ad26217c866ba7234a035136f8a3c9
Ojas <ojastulswani@gmail.com>
2023-10-11 12:56:39 +0530

    COMPONENTS CREATION STARTS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,102 @@
-import React,{useEffect} from 'react'
-import './App.css'
-import {BrowserRouter, Routes, Route } from 'react-router-dom'
-import { FiSettings } from 'react-icons/fi'
-import { TooltipComponent } from '@syncfusion/ej2-react-popups'
-
+import React, { useEffect } from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 const App = () => {
+  // active menu for side menu
+  const activeMenu = false;
+  // end of active menu logic
+
   return (
     <div>
+      {/* setting logo of page code starts here */}
       <BrowserRouter>
         <div className="flex relative dark:bg-main-dark-bg">
-          <div className="fixed right-4 bottom-4" style={{zIndex:'1000'}}>
-            <TooltipComponent content="Settings" position='Top'>
-              <button type = "button" className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white' style={{background: 'black', borderRadius:'50%' }}>
+          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+            <TooltipComponent content="Settings" position="Top">
+              <button
+                type="button"
+                className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
+                style={{ background: "blue", borderRadius: "50%" }}
+              >
                 <FiSettings />
               </button>
             </TooltipComponent>
           </div>
+          {/* ended the setting button part */}
+
+          {/* Sildebar for settings and all started  */}
+          {activeMenu ? (
+            <div className="w-72 fixed slidbar dark:bg-secondary-dark-bg bg-white  ">
+              Sildebar
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg ">Sildebar</div>
+          )}
+          {/* {slidsbar closes} */}
+
+          {/* navigation bar starts here  */}
+
+          <div
+            className={
+              activeMenu
+                ? "dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  "
+                : "bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 "
+            }
+          >
+            {/* created className as template literal and pass the active menu as a varible plus with tertinary  */}
+
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+              NAvbar
+            </div>
+          </div>
+
+          {/* navigation bar ends  */}
+
+          {/* main div for routing */}
+
+          <div>
+            <Routes>
+              {/* dashboard starts */}
+              <Route path="/" element="Ecommerce" />
+              <Route path="/ecommerce" element="Ecommerce" />
+              {/* ends  */}
+
+              {/* pages starts  */}
+
+              <Route path="/orders" element="Orders" />
+              <Route path="/employees" element="Employees" />
+              <Route path="/coustomer" element="Coustomer" />
+
+              {/* ends  */}
+
+              {/* Apps start  */}
+
+              <Route path="/kanban" element="Kanban" />
+              <Route path="/editor" element="Editor" />
+              <Route path="/calender" element="Calender" />
+              <Route path="/color-picker" element="ColorPicker" />
+
+              {/* Apps ends  */}
+
+              {/* charts */}
+              <Route path="/line" element="Line" />
+              <Route path="/area" element="Area" />
+              <Route path="/bar" element="Bar" />
+              <Route path="/pie" element="Pie" />
+              <Route path="/financial" element="Financial" />
+              <Route path="/color-mapping" element="ColorMapping" />
+              <Route path="/pyramid" element="Pyramid" />
+              <Route path="/stacked" element="Stacked" />
+              {/* ends  */}
+            </Routes>
+          </div>
         </div>
       </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
